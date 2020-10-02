e5254f071fc55acec7f78c4df7625a561b422dcd
baduraje@isu.edu <baduraje@isu.edu>
2020-10-01 22:59:22 -0600

    added onsuccess functionality in db.js

--- public/db.js
+++ public/db.js
@@ -40,7 +40,18 @@ request.onupgradeneeded = function(event) {
     const store = transaction.objectStore("pending");
     // get all records from store and set to a variable
     const getAll = store.getAll();
-  
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+          })
+
+          
 
 
 
